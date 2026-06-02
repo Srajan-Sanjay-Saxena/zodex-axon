30187e450222d843e630d6523e6b3a5fd8d6cf15
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-06-02 12:49:46 +0530

    feat: add ValidationError handler

---
+++ src/error/error.validation.controller.ts
@@ -0,0 +1,31 @@
+import type { Response } from "express";
+import { ApiError } from "@utils/api.error.js";
+import { BaseResponseClass } from "@utils/base.response.class.js";
+import type { ResponseData } from "@helper/types.helper.js";
+
+/**
+ * Handles Validation errors (HTTP 400).
+ * This class builds a Validation error response and sends it to the client.
+ */
+export class ValidationError<
+  TObj extends ResponseData,
+> extends BaseResponseClass<TObj, ApiError<TObj>> {
+  constructor() {
+    super(ApiError);
+  }
+
+  /**
+   * Builds and handles the Validation error response.
+   *
+   * @param {Response} res - The Express response object.
+   * @param {TObj} data - Optional additional data to include in the response.
+   * @returns {ApiError<TObj>} The created error response.
+   */
+  public override handleResponse(res: any, data?: TObj) {
+    return this.builderInstance
+      .setStatus(400)
+      .setMessage("Validation error occurred. Invalid fields given")
+      .setData(data as TObj)
+      .build(res) as ApiError<TObj>;
+  }
+}
