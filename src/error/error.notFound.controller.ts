cfa9924fd29d429c29028cc941b4d44fb8bb08f5
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-06-02 12:49:46 +0530

    feat: add NotFound error handler

---
+++ src/error/error.notFound.controller.ts
@@ -0,0 +1,32 @@
+import type { Response } from "express";
+import { ApiError } from "@utils/api.error.js";
+import { BaseResponseClass } from "@utils/base.response.class.js";
+import type { ResponseData } from "@helper/types.helper.js";
+
+/**
+ * Handles Not Found errors (HTTP 404).
+ * This class builds a NotFound error response and sends it to the client.
+ */
+export class NotFound<TObj extends ResponseData> extends BaseResponseClass<
+  TObj,
+  ApiError<TObj>
+> {
+  constructor() {
+    super(ApiError);
+  }
+
+  /**
+   * Builds and handles the NotFound error response.
+   *
+   * @param {Response} res - The Express response object.
+   * @param {TObj} data - Optional additional data to include in the response.
+   * @returns {ApiError<TObj>} The created error response.
+   */
+  public override handleResponse(res: Response, data?: TObj) {
+    return this.builderInstance
+      .setStatus(404)
+      .setMessage("Sorry request not found.")
+      .setData(data as TObj)
+      .build(res) as ApiError<TObj>;
+  }
+}
